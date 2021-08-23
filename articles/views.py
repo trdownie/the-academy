150f880b7971593ee50f823e5fcddcf1fa944038
trdownie <trdownie@gmail.com>
2021-08-23 12:25:50 +0000

    Added articles views and template and connected successfully

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Subject
+from .models import Subject, Article
 
 # Create your views here.
 
-def display_index(request):
-    return render(request, '')
+def all_articles(request):
+    """ view to show all articles, including sorting/searching """
+
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'articles/articles.html', context)
