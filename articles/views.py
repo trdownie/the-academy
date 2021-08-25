5fdcd8fe45ff26617e91af5fb8092d7108a570ea
trdownie <trdownie@gmail.com>
2021-08-25 15:14:10 +0000

    Added sorting functionality to articles.html, updated view accordingly

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Subject, Article
 
 # Create your views here.
@@ -11,14 +13,43 @@ def all_articles(request):
     articles = Article.objects.all()
     query = None
     subjects = None
-    
+    sort = None
+    direction = None
+    test_one = None # test - to remove ******************************************************
+    test_two = None # test - to remove ******************************************************
+
     if request.GET:
-        # determine whether a subject parameter is defined within request
+        # determine whether a sort parameter is defined
+        if 'sort' in request.GET:
+            # define sortkey as this parameter
+            sortkey = request.GET['sort']
+            # define sort as the same (to preserve sort)
+
+            # if sorting by article title, add a temporary lowercase field
+            # to the article model & update the sortkey accordingly
+            if sortkey == 'title':
+                sortkey = 'lowercase_title'
+                articles = articles.annotate(lowercase_title=Lower('title'))
+
+            # determine whether a direction parameter is defined
+            if 'direction' in request.GET:
+                # define direction as this parameter
+                direction = request.GET['direction']
+                # if direction is descending, sort by negative sortkey
+                # (since default is ascending and negative will reverse)
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            # if direction is descending, reverse sortkey
+            articles = articles.order_by(sortkey)
+
+        # determine whether a subject parameter is defined
         if 'subject' in request.GET:
             # if so, split this parameter into list
             subjects = request.GET['subject'].split(',')
             # find *distinct* articles with subject names in the list
-            articles = articles.filter(subjects__subject_name__in=subjects).distinct()
+            articles = articles.filter(
+                subjects__subject_name__in=subjects).distinct()
             # define a list of 'in use' subject names for later use
             subjects = Subject.objects.filter(subject_name__in=subjects)
 
@@ -36,6 +67,8 @@ def all_articles(request):
         'articles': articles,
         'search_term': query,
         'current_subjects': subjects,
+        'test_one': test_one,
+        'test_two': test_two,
     }
 
     return render(request, 'articles/articles.html', context)
