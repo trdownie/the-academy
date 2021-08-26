7f9a25089180ce927d38c4a0bd2e4efce594512f
trdownie <trdownie@gmail.com>
2021-08-26 12:14:54 +0000

    Added following count to academic profile

--- academics/views.py
+++ academics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models import Count
 
 from .models import Academic
 
@@ -10,9 +11,13 @@ def academic_profile(request, academic_id):
     """ view to show individual article details """
 
     academic = get_object_or_404(Academic, pk=academic_id)
+    # academics = Academic.objects.all()
+
+    followers = academic.academic_set.all().count()
 
     context = {
         'academic': academic,
+        'followers': followers,
     }
 
     return render(request, 'academics/academic_profile.html', context)
