040e1a5423068c22c4a31156361223ab42725094
trdownie <trdownie@gmail.com>
2021-08-26 18:39:37 +0000

    Added add to bag functionality on article_detail page and connected via view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -13,3 +13,17 @@ def shopping_bag(request):
     }
 
     return render(request, 'bag/bag.html', context)
+
+
+def add_to_bag(request, article_id):
+    """ Add specified article to shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    bag[article_id] = 1
+
+    request.session['bag'] = bag
+    
+    print(request.session['bag'])
+    return redirect(redirect_url)
