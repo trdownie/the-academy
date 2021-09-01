13b68ba51549fcaf2985a807a58a8e75b9f69e06
trdownie <trdownie@gmail.com>
2021-09-01 17:44:33 +0000

    Connected an (unstyled) Stripe payment form

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JUwwKI6dywDr6yKBHUYgU7RmbufrYpnBW8j0IRAazOW5oo6kS54uMa5ZNEfeFQVu8aWEllmEOYlJd4UuOTjXebx00oauOUumX',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
