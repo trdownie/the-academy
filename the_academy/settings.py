a0f2e072867d803831a131ab6c47b23bdb6b3b20
trdownie <trdownie@gmail.com>
2021-09-01 19:43:00 +0000

    Added basic checkout functionality (unstyled) & confirmed connected to Stripe

--- the_academy/settings.py
+++ the_academy/settings.py
@@ -181,5 +181,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.get.env('STRIPE_PUBLIC_KEY', '')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
