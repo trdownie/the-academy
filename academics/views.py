013ba04d31adf34d2ba39998c357c9691e0be7aa
trdownie <trdownie@gmail.com>
2021-09-07 14:57:01 +0000

    Added crispy form to profile template (unstyled) & connected via view

--- academics/views.py
+++ academics/views.py
@@ -3,21 +3,22 @@ from django.contrib import messages
 from django.db.models import Count
 
 from .models import Academic
-
-# Create your views here.
+from .forms import AcademicProfileForm
 
 
 def academic_profile(request, academic_id):
     """ view to show individual article details """
-
     academic = get_object_or_404(Academic, pk=academic_id)
     # academics = Academic.objects.all()
-
     followers = academic.academic_set.all().count()
+    form = AcademicProfileForm(instance=academic)
+    orders = academic.orders.all()
 
     context = {
         'academic': academic,
         'followers': followers,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, 'academics/academic_profile.html', context)
