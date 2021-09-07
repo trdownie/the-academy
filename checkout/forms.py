1cd0f9fcca87c311fa22144b41bc480933fa71d3
trdownie <trdownie@gmail.com>
2021-09-07 10:37:17 +0000

    Added country field dropdown to Order model/payment form

--- checkout/forms.py
+++ checkout/forms.py
@@ -24,15 +24,15 @@ def __init__(self, *args, **kwargs):
             'town_or_city': 'Town or City',
             'postcode': 'Post Code',
             'county': 'County',
-            'country': 'Country',
     }
 
     self.fields['full_name'].widget.attrs['autofocus'] = True
     for field in self.fields:
-        if self.fields[field].required:
-            placeholder = f'{placeholders[field]} *'
-        else:
-            placeholder = placeholders[field]
+        if field != 'country':
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
         self.fields[field].widget.attrs['placeholder'] = placeholder
         self.fields[field].widget.attrs['class'] = 'stripe-style-input'
         self.fields[field].label = False
