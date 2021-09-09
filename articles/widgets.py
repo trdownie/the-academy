6a5b3a6f66616c7ab55c587b0b1549f59c3c4abc
trdownie <trdownie@gmail.com>
2021-09-09 13:18:32 +0000

    Minor styling on nav form input to seperate from form input elsewhere

--- articles/widgets.py
+++ articles/widgets.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove')
+    clear_checkbox_label = _('Remove Image')
     initial_text = _('Current Image')
     input_text = _('')
-    template_name = 'articles/custom_widget_templates/custom_clearable_file_input.html'
+    template_name = 'articles/custom_widget_templates/custom_clearable_file_input.html'  # NOT WORKING!
