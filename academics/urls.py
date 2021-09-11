03b4fd5c0e647894d5e97fd0a6c25e641d454be9
trdownie <trdownie@gmail.com>
2021-09-11 18:06:06 +0000

    Added follow/unfollow functionality to profile including logic (working)

--- academics/urls.py
+++ academics/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('<academic_id>', views.academic_profile, name='academic_profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('<int:article_id>/', views.article_detail_profile, name='article_detail_profile'),
+    path('follow/<academic_id>', views.follow, name='follow'),
+    path('unfollow/<academic_id>', views.unfollow, name='unfollow'),
 ]
--- academics/views.py
+++ academics/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from time import sleep
+
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from .models import Academic
 from .forms import AcademicProfileForm
 from checkout.models import Order
 from articles.models import Article
+
 
 def academic_profile(request, academic_id):
     """ view to show individual article details """
@@ -20,7 +23,8 @@ def academic_profile(request, academic_id):
     else:
         form = AcademicProfileForm(instance=academic)
     # academics = Academic.objects.all()
-    followers = academic.academic_set.all().count()
+    followers = academic.academic_set.all()
+    follower_count = followers.count()
     form = AcademicProfileForm(instance=academic)
     orders = academic.orders.all()
     articles = academic.article_set.all()
@@ -28,6 +32,7 @@ def academic_profile(request, academic_id):
     context = {
         'academic': academic,
         'followers': followers,
+        'follower_count': follower_count,
         'form': form,
         'orders': orders,
         'on_profile': True,
@@ -65,3 +70,76 @@ def article_detail_profile(request, article_id):
     }
 
     return render(request, 'articles/article_detail.html', context)
+
+
+def follow(request, academic_id):
+    """ follow another academic """
+    academic = Academic.objects.get(user=request.user)
+    print(academic_id)
+    current_profile = Academic.objects.get(pk=academic_id)
+    print(current_profile)
+
+    # follow academic whose profile user is on - WORKING
+    if academic_id == academic.id:
+        messages.error(request, "You can't follow yourself!")
+    else:
+        to_follow = get_object_or_404(Academic, pk=academic_id)
+        if academic.following.filter(id=to_follow.id).exists():
+            messages.error(request, f"You're already following {to_follow}!")
+        else:
+            academic.following.add(to_follow)
+            academic.save()
+
+    followers = academic.academic_set.all()
+    follower_count = followers.count()
+    form = AcademicProfileForm(instance=academic)
+    orders = academic.orders.all()
+    articles = academic.article_set.all()
+
+    context = {
+        'academic': current_profile,
+        'followers': followers,
+        'follower_count': follower_count,
+        'form': form,
+        'orders': orders,
+        'on_profile': True,
+        'articles': articles,
+        'following': True,
+    }
+
+    return render(request, 'academics/academic_profile.html', context)
+
+
+def unfollow(request, academic_id):
+    """ follow another academic """
+    academic = Academic.objects.get(user=request.user)
+    print(academic_id)
+    current_profile = Academic.objects.get(pk=academic_id)
+    print(current_profile)
+
+    to_unfollow = get_object_or_404(Academic, pk=academic_id)
+    if academic.following.filter(id=to_unfollow.id).exists():
+        academic.following.remove(to_unfollow)
+        academic.save()
+        messages.error(request, f"You're no longer following {to_unfollow}.")
+    else:
+        messages.error(request, f"You're not following {to_unfollow}!")
+
+    followers = academic.academic_set.all()
+    follower_count = followers.count() - 1
+    form = AcademicProfileForm(instance=academic)
+    orders = academic.orders.all()
+    articles = academic.article_set.all()
+
+    context = {
+        'academic': current_profile,
+        'followers': followers,
+        'follower_count': follower_count,
+        'form': form,
+        'orders': orders,
+        'on_profile': True,
+        'articles': articles,
+        'following': False,
+    }
+
+    return render(request, 'academics/academic_profile.html', context)
