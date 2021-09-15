2a91bd144ef5b9e0e723fe6fb84baa1f5b48a944
trdownie <trdownie@gmail.com>
2021-09-15 18:07:21 +0000

    Added limitations so users can only edit own articles unless superuser

--- articles/views.py
+++ articles/views.py
@@ -7,72 +7,80 @@ from django.db.models.functions import Lower
 from .models import Subject, Article
 from .forms import ArticleForm
 
-# Create your views here.
 
 def all_articles(request):
-    """ view to show all articles, including sorting/searching """
+    """View to show all articles, including filtering/sorting/searching"""
 
+    # Get all articles
     articles = Article.objects.all()
+    # Set filtering/sorting/searching criteria to none
     query = None
     subjects = None
     sort = None
     direction = None
     proposals = False
 
+    # On a get request
     if request.GET:
 
+        # If 'proposals' exists (via the proposals toggle)
+        # then filter articles accordingly
         if 'proposals' in request.GET:
             articles = Article.objects.filter(proposal=True)
             proposals = True
             messages.info(request, f'Showing proposals only')
 
-        # determine whether a sort parameter is defined
+        # If a sort parameter is defined
         if 'sort' in request.GET:
-            # define sortkey as this parameter
+            # Define sortkey as this parameter
             sortkey = request.GET['sort']
-            sort = sortkey
-            # define sort as the same (to preserve sort)
+            sort = sortkey  # (to preserve sort)
 
-            # if sorting by article title, add a temporary lowercase field
+            # If sorting by article title, add a temporary lowercase field
             # to the article model & update the sortkey accordingly
             if sortkey == 'title':
                 sortkey = 'lowercase_title'
                 articles = articles.annotate(lowercase_title=Lower('title'))
 
-            # determine whether a direction parameter is defined
+            # Determine whether a direction parameter is defined
             if 'direction' in request.GET:
-                # define direction as this parameter
+                # Define direction as this parameter
                 direction = request.GET['direction']
-                # if direction is descending, sort by negative sortkey
+                # If direction is descending, sort by negative sortkey
                 # (since default is ascending and negative will reverse)
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
 
-            # if direction is descending, reverse sortkey
+            # If direction is descending, reverse sortkey
             articles = articles.order_by(sortkey)
-            messages.info(request, f'Articles sorted by {sort} in {direction}ending order')
+            messages.info(request, f'Articles sorted by {sort} \
+                          in {direction}ending order')
 
-        # determine whether a subject parameter is defined
+        # Determine whether a subject parameter is defined
         if 'subject' in request.GET:
-            # if so, split this parameter into list
+            # If so, split this parameter into list
             subjects = request.GET['subject'].split(',')
-            # find *distinct* articles with subject names in the list
+            # Find *distinct* articles with subject names in the list
             articles = articles.filter(
                 subjects__subject_name__in=subjects).distinct()
-            # define a list of 'in use' subject names for later use
+            # Define a list of 'in use' subject names for later use
             subjects = Subject.objects.filter(subject_name__in=subjects)
 
-
+        # Determine whether a search parameter is defined
         if 'q' in request.GET:
+            # If so, obtain this
             query = request.GET['q']
+            # If nothing entered, display error message
             if not query:
                 messages.error(request, "No search criteria entered!")
                 return redirect(reverse('articles'))
-            
+
+            # Obtain articles containing query
             queries = Q(title__icontains=query) | Q(summary__icontains=query)
             articles = articles.filter(queries)
             messages.info(request, f'Showing articles containing: {query}')
 
+    template = 'articles/articles.html'
     context = {
         'articles': articles,
         'search_term': query,
@@ -80,7 +88,7 @@ def all_articles(request):
         'proposals': proposals,
     }
 
-    return render(request, 'articles/articles.html', context)
+    return render(request, template, context)
 
 
 def article_detail(request, article_id):
@@ -88,25 +96,32 @@ def article_detail(request, article_id):
 
     article = get_object_or_404(Article, pk=article_id)
 
+    template = 'articles/article_detail.html'
     context = {
         'article': article,
     }
 
-    return render(request, 'articles/article_detail.html', context)
+    return render(request, template, context)
 
 
 @login_required
 def add_article(request):
     """Add new article to store"""
 
+    # Upon the form submitting (POST)
     if request.method == 'POST':
+        # Obtain the form details using the ArticleForm() form
         form = ArticleForm(request.POST, request.FILES)
+        # If the form is valid, save it and return to the article detail page
         if form.is_valid():
             article = form.save()
             messages.success(request, 'Article uploaded!')
             return redirect(reverse('article_detail', args=[article.id]))
+        # Otherwise, return an error message
         else:
-            messages.error(request, 'Article failed to upload. Please check form!')
+            messages.error(request, 'Article failed to upload. \
+                           Please check form!')
+    # Upon usual (non-POST) function request, define form using ArticleForm()
     else:
         form = ArticleForm()
 
@@ -117,14 +132,17 @@ def add_article(request):
 
     return render(request, template, context)
 
+
 @login_required
 def edit_article(request, article_id):
     """Edit article"""
-    if not request.user.is_superuser:
-        messages.error(request, 'This area is for contributing academics only!')
-        return redirect(reverse('home'))
 
     article = get_object_or_404(Article, pk=article_id)
+
+    if not article.authors.filter(user=request.user).exists():
+        if not request.user.is_superuser:
+            messages.error(request, 'Area for senior academics only!')
+            return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ArticleForm(request.POST, request.FILES, instance=article)
@@ -133,7 +151,8 @@ def edit_article(request, article_id):
             messages.success(request, 'Article updated!')
             return redirect(reverse('article_detail', args=[article.id]))
         else:
-            messages.error(request, 'Article failed to update. Please check form!')
+            messages.error(request, 'Article failed to update. \
+                           Please check form!')
     else:
         form = ArticleForm(instance=article)
         messages.info(request, f'You are now editing {article.title}')
@@ -150,7 +169,7 @@ def edit_article(request, article_id):
 def delete_article(request, article_id):
     """Edit article"""
     if not request.user.is_superuser:
-        messages.error(request, 'This area is for contributing academics only!')
+        messages.error(request, 'Area for contributing academics only!')
         return redirect(reverse('home'))
 
     article = get_object_or_404(Article, pk=article_id)
