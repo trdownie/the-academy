184f0fd98d2c42d84c81913e95920458e7b654fe
trdownie <trdownie@gmail.com>
2021-09-16 15:43:40 +0000

    Removed secret key and set debug

--- the_academy/settings.py
+++ the_academy/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!j7pav7f7b(2z7_5*-!sqz*gk17x6udrpwgb@spnxqp@qfe+3v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-home-of-learning.herokuapp.com', 'localhost']
 
