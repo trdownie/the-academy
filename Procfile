44f6ca30228d42ad663f499857b4e7e16c5a2669
trdownie <trdownie@gmail.com>
2021-09-16 14:32:13 +0000

    Deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the-academy.wsgi:application
--- the_academy/settings.py
+++ the_academy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!j7pav7f7b(2z7_5*-!sqz*gk17x6udrpwgb@spnxqp@qfe+3v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-home-of-learning.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,18 @@ WSGI_APPLICATION = 'the_academy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
